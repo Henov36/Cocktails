861d2e12b9cf9c03b4f392f35857296a33c84619
Henov36 <fiz.ryk.5656@gmail.com>
2025-08-09 16:12:12 +0300

    avif images

--- main.js
+++ main.js
@@ -1,157 +1,10 @@
-// import { animatedSlide } from "./gsap.js";
-
-// const sliderLists = [
-// 	{
-// 		id: 1,
-// 		name: "Classic Mojito",
-// 		image: "./img/coctailSlide/classic.png",
-// 		title: "Simple Ingredients, Bold Flavor	",
-// 		description:
-// 			"Made with tequila, lime juice, and orange liqueur, the Margarita is easy to make and full of character. Add a salted rim for the perfect drink on summer nights.",
-// 	},
-// 	{
-// 		id: 2,
-// 		name: "Raspberry Mojito",
-// 		image: "./img/coctailSlide/1.png",
-// 		title: "A Zesty Classic That Never Fails",
-// 		description:
-// 			"The Margarita is a classic that balances tangy lime, smooth tequila, and a touch of sweetness. Shaken, frozen, or on the rocks—it’s always crisp & refreshing.",
-// 	},
-// 	{
-// 		id: 3,
-// 		name: "Violet Breeze",
-// 		image: "./img/coctailSlide/violet.png",
-// 		title: "Simple Ingredients, Bold Flavor",
-// 		description:
-// 			"Made with tequila, lime juice, and orange liqueur, the Margarita is easy to make and full of character. Add a salted rim for the perfect drink on summer nights.",
-// 	},
-// 	{
-// 		id: 4,
-// 		name: "Curacao Mojito",
-// 		image: "./img/coctailSlide/tipsy.png",
-// 		title: "Crafted With Care, Poured With Love",
-// 		description:
-// 			"Each cocktail is made with fresh ingredients and a passion for perfecting every pour, whether you're celebrating or simply relaxing.",
-// 	},
-// ];
-// const slideContainer = document.querySelector(".slide__nav");
-// const cocktailItem = document.querySelector(".coctail__item");
-
-// sliderLists.forEach((item) => {
-// 	const htmlSlide = `
-// 					<button class="slide__nav-buttons" data-id="${item.id}">
-// 						<h5>${item.name}</h5>
-// 					</button>
-// 		`;
-// 	slideContainer.insertAdjacentHTML("beforeend", htmlSlide);
-// });
-
-// function renderSlide(id, direction = "right") {
-// 	const item = sliderLists.find((slide) => slide.id === id);
-// 	if (!item) return;
-
-// 	const currentIndex = sliderLists.findIndex((slide) => slide.id === id);
-// 	const prevIndex =
-// 		(currentIndex - 1 + sliderLists.length) % sliderLists.length;
-// 	const nextIndex = (currentIndex + 1) % sliderLists.length;
-
-// 	const prevItem = sliderLists[prevIndex];
-// 	const nextItem = sliderLists[nextIndex];
-
-// 	cocktailItem.innerHTML = `
-// 		<div class="slide-content">
-// 			<div class="img-block">
-// 				<img class="animated-image c${item.id}" src="${item.image}" alt="${item.name}" />
-// 				<span class="cocktail-arrows left-arrow left-span">
-// 				<p class="left-text">${prevItem.name}</p>
-
-// 				<img
-// 					src="./img/coctailSlide/arrow.svg"
-// 					class="cocktail-arrow-left"
-// 					alt="Left" />
-// 			</span>
-// 			<span class="cocktail-arrows right-arrow right-span">
-// 			<p class="right-text">${nextItem.name}</p>
-
-// 				<img
-// 					src="./img/coctailSlide/arrow.svg"
-// 					class="cocktail-arrow-right"
-// 					alt="Right" />
-// 			</span>
-// 			</div>
-// 			<div class="slide__content">
-// 				<div class="margin-block">
-// 					<span>Recipes for:</span>
-// 					<h5>${item.name}</h5>
-// 				</div>
-// 				<div class="slide-des">
-// 					<h2>${item.title}</h2>
-// 					<p>${item.description}</p>
-// 				</div>
-// 			</div>
-
-// 		</div>
-// 	`;
-
-// 	animatedSlide(id, direction);
-
-// 	// 👉 Добавим слушатели на только что отрендеренные стрелки:
-// 	const newLeftArrow = document.querySelector(".left-span");
-// 	const newRightArrow = document.querySelector(".right-span");
-
-// 	newRightArrow.addEventListener("click", (e) => {
-// 		e.preventDefault();
-// 		currentSlide = currentSlide + 1 > sliderLists.length ? 1 : currentSlide + 1;
-// 		updateActiveSlide();
-// 		renderSlide(currentSlide, "right");
-// 	});
-
-// 	newLeftArrow.addEventListener("click", (e) => {
-// 		e.preventDefault();
-// 		currentSlide = currentSlide - 1 < 1 ? sliderLists.length : currentSlide - 1;
-// 		updateActiveSlide();
-// 		renderSlide(currentSlide, "left");
-// 	});
-// }
-
-// const slideNavButtons = document.querySelectorAll(".slide__nav-buttons");
-// let currentSlide = 1;
-
-// renderSlide(currentSlide);
-
-// function updateActiveSlide() {
-// 	slideNavButtons.forEach((button) => {
-// 		const buttonId = parseInt(button.getAttribute("data-id"));
-
-// 		if (buttonId === currentSlide) {
-// 			button.classList.add("active-button");
-// 		} else {
-// 			button.classList.remove("active-button");
-// 		}
-// 	});
-// }
-
-// updateActiveSlide();
-
-// slideNavButtons.forEach((button) => {
-// 	const buttonId = parseInt(button.getAttribute("data-id"));
-
-// 	button.addEventListener("click", (e) => {
-// 		e.preventDefault();
-// 		if (currentSlide === buttonId) return;
-// 		currentSlide = buttonId;
-// 		updateActiveSlide();
-// 		renderSlide(currentSlide);
-// 	});
-// });
-
 import { animatedSlide } from "./gsap.js";
 
 const sliderLists = [
 	{
 		id: 1,
 		name: "Classic Mojito",
-		image: "./img/coctailSlide/classic.png",
+		image: "./img/coctailSlide/classic.avif",
 		title: "Simple Ingredients, Bold Flavor",
 		description:
 			"Made with tequila, lime juice, and orange liqueur, the Margarita is easy to make and full of character. Add a salted rim for the perfect drink on summer nights.",
@@ -159,7 +12,7 @@ const sliderLists = [
 	{
 		id: 2,
 		name: "Raspberry Mojito",
-		image: "./img/coctailSlide/1.png",
+		image: "./img/coctailSlide/1.avif",
 		title: "A Zesty Classic That Never Fails",
 		description:
 			"The Margarita is a classic that balances tangy lime, smooth tequila, and a touch of sweetness. Shaken, frozen, or on the rocks—it’s always crisp & refreshing.",
@@ -167,7 +20,7 @@ const sliderLists = [
 	{
 		id: 3,
 		name: "Violet Breeze",
-		image: "./img/coctailSlide/violet.png",
+		image: "./img/coctailSlide/violet.avif",
 		title: "Simple Ingredients, Bold Flavor",
 		description:
 			"Made with tequila, lime juice, and orange liqueur, the Margarita is easy to make and full of character. Add a salted rim for the perfect drink on summer nights.",
@@ -175,7 +28,7 @@ const sliderLists = [
 	{
 		id: 4,
 		name: "Curacao Mojito",
-		image: "./img/coctailSlide/tipsy.png",
+		image: "./img/coctailSlide/tipsy.avif",
 		title: "Crafted With Care, Poured With Love",
 		description:
 			"Each cocktail is made with fresh ingredients and a passion for perfecting every pour, whether you're celebrating or simply relaxing.",
